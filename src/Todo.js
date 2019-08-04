a3d6f1e7bb0df86b989f507eaff8e1c29e159acf
Alan Xavier <alan_xvr@hotmail.com>
2019-08-04 16:12:43 -0400

    Consume the Todo Context

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import EditTodoForm from "./EditTodoForm";
 import useToggle from "./hooks/useToggle";
 import ListItem from "@material-ui/core/ListItem";
@@ -8,18 +8,15 @@ import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
+import { TodosContext } from "./context/todos.context";
 
-function Todo({ id, task, completed, removeTodo, toggleTodo, editTodo }) {
+function Todo({ id, task, completed }) {
+  const { removeTodo, toggleTodo } = useContext(TodosContext);
   const [isEditing, toggleIsEditting] = useToggle(false);
   return (
     <ListItem style={{ height: "64px" }}>
       {isEditing ? (
-        <EditTodoForm
-          id={id}
-          task={task}
-          editTodo={editTodo}
-          toggleIsEditting={toggleIsEditting}
-        />
+        <EditTodoForm id={id} task={task} toggleIsEditting={toggleIsEditting} />
       ) : (
         <>
           <Checkbox
