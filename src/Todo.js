6377e3ffd29c69e19f8268204995d54294f3c634
Alan Xavier <alan_xvr@hotmail.com>
2019-08-04 17:52:30 -0400

    Split Contexts

--- src/Todo.js
+++ src/Todo.js
@@ -8,10 +8,10 @@ import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
-import { TodosContext } from "./context/todos.context";
+import { DispatchContext } from "./context/todos.context";
 
 function Todo({ id, task, completed }) {
-  const { dispatch } = useContext(TodosContext);
+  const dispatch = useContext(DispatchContext);
   const [isEditing, toggleIsEditting] = useToggle(false);
   return (
     <ListItem style={{ height: "64px" }}>
